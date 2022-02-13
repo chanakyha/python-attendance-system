b080b993e1d33a3d5d9eb28c5256ad2ba24275ac
Chanakyha <66877639+chanakyha@users.noreply.github.com>
2022-02-13 19:36:46 +0530

    Finished adding the main screen table and imported some modules

---
+++ main.py
@@ -0,0 +1,23 @@
+from prettytable import PrettyTable
+from termcolor import colored
+import os
+
+
+def clrscr():
+    os.system("cls") | os.system("clear")
+
+
+# Clearing the screen brfore starting
+clrscr()
+
+print(colored("WELCOME TO SCHOOL ATTENDANCE SYSTEM", "white", "on_red"))
+mainTable = PrettyTable(["Enter", "Choice"])
+mainTable.add_row(["Teacher's Login", "1"])
+mainTable.add_row(["Student's Login", "2"])
+mainTable.add_row(["Create Account", "3"])
+print(mainTable)
+
+choice = None
+
+while (not choice):
+    choice = input("Enter the choice: ")
